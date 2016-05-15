a17b6ae0fb12c5e347678636abb4451cc7875a7e
xwan193 <xwan193@student.monash.edu>
2016-05-15 19:41:14 +1000

    addlocation page edited

---
+++ code/js/addlocationPage.js
@@ -0,0 +1,22 @@
+// Code for the Add Location page.
+ function outputLocationA(){
+        var x,y,LocationA;
+            x=document.getElementById("form1");
+            y=x.elements["LocationA"].value;
+            
+         //   document.write(y);
+            document.getElementById("demo").innerHTML=y;
+    
+        }
+
+
+
+ function outputLocationB(){
+        var x1,y1,LocationB;
+            x1=document.getElementById("form2");
+            y1=x1.elements["LocationB"].value;
+            
+         //   document.write(y);
+            document.getElementById("demo2").innerHTML=y1;
+    
+        }
